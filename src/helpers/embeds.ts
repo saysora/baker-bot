fb085126dd8721f075f25e053d6e3cbdbc5159bb
sora <braden@devskull.com>
2025-12-01 13:18:49 -0800

    One more line break

--- src/helpers/embeds.ts
+++ src/helpers/embeds.ts
@@ -42,7 +42,7 @@ export function gatherEmbed(
     .setColor(Color.recipe)
     .setThumbnail(user.displayAvatarURL());
 
-  let description = `### 🔎 Gather Results\nFor <@${user.id}>\n`;
+  let description = `### 🔎 Gather Results\nFor <@${user.id}>\n\n`;
 
   for (const ing in newIngredients) {
     const ingredientCount = newIngredients[ing];
